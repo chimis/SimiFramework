8c09857b37c7ccc720cf41009358d2a2fff81ad4
chimis <chimis@foxmail.com>
2016-01-20 21:40:40 +0800

    框架入口

---
+++ framework/src/main/java/io/simi/app/SimiFramework.java
@@ -0,0 +1,32 @@
+package io.simi.app;
+
+import android.content.Context;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+
+import io.simi.utils.Utils;
+
+/**
+ * Code: 4.0.3(min-sdk) & 6.0(target-sdk)
+ * Creator: chimis
+ * Created time: 16/1/18 18:36
+ * Updated time: 16/1/18 18:36
+ */
+public class SimiFramework {
+
+    private static SimiFramework instance;
+    private Context context;
+
+    private SimiFramework(Context context) {
+        this.context = context;
+    }
+
+    public static void initialize(Context context) {
+        if (instance == null) {
+            instance = new SimiFramework(context);
+        }
+        Utils.initialize(instance.context);
+        Fresco.initialize(instance.context);
+    }
+
+}
